5f42ad3a3903129cfb150721468402d12b1d8b5b
idoco <ido.c.mail@gmail.com>
2017-12-07 10:49:08 -0800

    location demo page

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const path    = require("path");
 const geoip = require('geoip-lite');
 const Loadmill = require('express-loadmill');
 
 const app = express();
 app.use(Loadmill({verifyToken: process.env.LOADMILL_VERIFY_TOKEN}));
 
-app.get('/', (req, res) => {
+app.get('/location', (req, res) => {
     try {
 
         let ip = req.headers['x-forwarded-for'] || "";
         ip = ip.split(",")[0];
-        console.log("IP is ",geoip.pretty(ip));
 
         const location = geoip.lookup(ip);
         console.log("Location is ", location);
-        res.send(location);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(location));
 
     } catch (e) {
         console.log("error: ", e);
@@ -22,4 +23,8 @@ app.get('/', (req, res) => {
     }
 });
 
-app.listen(80, () => console.log('Example app listening on port 80!'));
+app.get('/',  (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000, () => console.log('Example app listening on port 80!'));
